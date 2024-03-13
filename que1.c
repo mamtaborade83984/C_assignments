1c486b12476257c528ae56b54889d988f969a7b1
mamtaborade <103098767+mamtaborade@users.noreply.github.com>
2024-03-13 14:02:48 +0530

    Add files via upload

---
+++ que1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,add,dif,pro;
+	printf("Enter 1st no :");
+	scanf("%d",&a);
+	printf("Enter 2nd  no :");
+	scanf("%d",&b);
+	add=a+b;
+	printf("Addition is =%d\n",add);
+
+	dif=a-b;
+	printf("difference is =%d\n",dif);
+
+	pro=a*b;
+	printf("Product is =%d\n",pro);
+
+	return 0;
+}
